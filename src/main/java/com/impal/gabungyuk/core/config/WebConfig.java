5665dfd7df2c3ee3c4cdab34a0b9054ecdaa764e
Akbar Rizqullah Putra Susanto <akbarrizqullah228@gmail.com>
2026-05-06 01:57:53 +0700

    fix (chore) [AR]: fixing chore url image profil

--- src/main/java/com/impal/gabungyuk/core/config/WebConfig.java
+++ src/main/java/com/impal/gabungyuk/core/config/WebConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadDir = System.getProperty("user.home") + "/uploads/";
+
+        String path = Paths.get("uploads").toAbsolutePath().toUri().toString();
 
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadDir);
+                .addResourceLocations(path);
     }
 }

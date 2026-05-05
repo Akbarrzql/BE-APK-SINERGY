59f9a9d6c5852ff74c1885527574be9f807c8c06
RAIHAN WENDRA BASWARA <baswarastudy@gmail.com>
2026-05-05 18:32:13 +0700

    fix(profile) [RWB]: update edit profile to support multipart, null strings, and social media fields [GI-43, GI-46, GI-47]

--- src/main/java/com/impal/gabungyuk/auth/model/request/UpdateUserRequest.java
+++ src/main/java/com/impal/gabungyuk/auth/model/request/UpdateUserRequest.java
@@ -4,20 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UpdateUserRequest {
-    // private String username; diubah karena sekarang pake nama lengkap
     private String email;
     private String password;
     private String namaLengkap;
-    private String profilePicture;
     private String institusi;
     private String bio;
-    private String keahlian;
+    private List<String> keahlian;
     private String lokasi;
     private String whatsapp;
-    }
+    private String instagram;
+    private String facebook;
+    private String linkedin;
+}
